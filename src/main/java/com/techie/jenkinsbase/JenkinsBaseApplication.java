410e10c7d17dd202c74c3a497ee09403b7087780
girish-thetechieguy <girish.thetechieguy@gmail.com>
2023-04-07 08:39:39 +0800

    added the base project

---
+++ src/main/java/com/techie/jenkinsbase/JenkinsBaseApplication.java
@@ -0,0 +1,13 @@
+package com.techie.jenkinsbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techie/jenkinsbase/JenkinsBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techie.jenkinsbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
